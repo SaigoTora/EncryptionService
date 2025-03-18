37a7946700e2462c59f6a160186599d673b9a2c8
Bohdan Kalnytskyi <bogdan201375@gmail.com>
2025-03-18 16:42:21 +0200

    Service and encryption key created.

---
+++ EncryptionService.Core/Models/VerticalTranspositionKey.cs
@@ -0,0 +1,9 @@
+﻿using EncryptionService.Core.Interfaces;
+
+namespace EncryptionService.Core.Models
+{
+	public class VerticalTranspositionKey : IEncryptionKey<string>
+	{
+		public required string Key { get; init; }
+	}
+}
---
+++ EncryptionService.Core/Services/VerticalTranspositionEncryptionService.cs
@@ -0,0 +1,19 @@
+﻿using EncryptionService.Core.Interfaces;
+using EncryptionService.Core.Models;
+
+namespace EncryptionService.Core.Services
+{
+	public class VerticalTranspositionEncryptionService
+		: IEncryptionService<VerticalTranspositionKey, string>
+	{
+		public EncryptionResult Encrypt(string text, VerticalTranspositionKey encryptionKey)
+		{
+			throw new NotImplementedException();
+		}
+		public EncryptionResult Decrypt(string encryptedText, VerticalTranspositionKey encryptionKey)
+		{
+			throw new NotImplementedException();
+		}
+
+	}
+}
