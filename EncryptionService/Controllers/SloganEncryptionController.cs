3163b677eabbdb7d4fd3e2a9a59d38d25b0ed8bd
Bohdan Kalnytskyi <bogdan201375@gmail.com>
2025-03-30 19:21:42 +0300

    The initial view and the controller are created.

---
+++ EncryptionService/Controllers/SloganEncryptionController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+using EncryptionService.Configurations;
+using EncryptionService.Core.Interfaces;
+using EncryptionService.Core.Models.SloganEncryption;
+using EncryptionService.Models;
+
+namespace EncryptionService.Controllers
+{
+	public class SloganEncryptionController(
+		IEncryptionService<SloganEncryptionResult, SloganEncryptionKey, string> encryptionService,
+		IOptions<EncryptionSettings> encryptionSettings)
+		: Controller
+	{
+		readonly IEncryptionService<SloganEncryptionResult, SloganEncryptionKey, string>
+			_encryptionService = encryptionService;
+		readonly EncryptionSettings _encryptionSettings = encryptionSettings.Value;
+
+		public IActionResult Index() => View();
+
+		[HttpPost]
+		public IActionResult Index(EncryptionViewModel<SloganEncryptionResult> encryptionViewModel,
+			string actionType)
+		{
+			if (!ModelState.IsValid)
+				return View(encryptionViewModel);
+
+			SloganEncryptionKey key = _encryptionSettings.SloganEncryptionKey;
+			SloganEncryptionResult encryptionResult;
+
+			if (actionType == "Encrypt")
+			{
+				encryptionResult = _encryptionService.Encrypt(encryptionViewModel.InputText!, key);
+				encryptionViewModel.EncryptionResult = encryptionResult;
+			}
+			else if (actionType == "Decrypt")
+			{
+				encryptionResult = _encryptionService.Decrypt(
+					encryptionViewModel.EncryptedInputText!, key);
+				encryptionViewModel.DecryptionResult = encryptionResult;
+			}
+
+			return View(encryptionViewModel);
+		}
+	}
+}
