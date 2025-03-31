3fbff9ebb61fc97c7a666594afe869afd8697580
Bohdan Kalnytskyi <bogdan201375@gmail.com>
2025-03-31 15:07:37 +0300

    Encryption and decryption methods have been implemented.

--- EncryptionService.Core/Services/HomophonicEncryptionService.cs
+++ EncryptionService.Core/Services/HomophonicEncryptionService.cs
@@ -1,4 +1,6 @@
-﻿using EncryptionService.Core.Interfaces;
+﻿using System.Text;
+
+using EncryptionService.Core.Interfaces;
 using EncryptionService.Core.Models;
 using EncryptionService.Core.Models.HomophonicEncryption;
 
@@ -10,11 +12,59 @@ namespace EncryptionService.Core.Services
 	{
 		public EncryptionResult Encrypt(string text, HomophonicEncryptionKey encryptionKey)
 		{
-			return new EncryptionResult("encrypted text");
+			text = text.ToUpper();
+			Random random = new();
+			StringBuilder builder = new();
+			Dictionary<char, List<int>> keyCopy = GenerateKeyCopy(encryptionKey);
+
+			foreach (char ch in text)
+			{
+				if (keyCopy[ch].Count != 0)
+				{
+					List<int> numbers = keyCopy[ch];
+					int randomIndex = random.Next(0, numbers.Count);
+					builder.Append(numbers[randomIndex].ToString("D3"));
+					numbers.RemoveAt(randomIndex);
+				}
+				else
+				{
+					int[] numbers = encryptionKey.Key[ch];
+					int randomIndex = random.Next(0, numbers.Length);
+					builder.Append(numbers[randomIndex].ToString("D3"));
+				}
+			}
+
+			return new EncryptionResult(builder.ToString());
 		}
-		public EncryptionResult Decrypt(string encryptedText, HomophonicEncryptionKey encryptionKey)
+		public EncryptionResult Decrypt(string encryptedText,
+			HomophonicEncryptionKey encryptionKey)
 		{
-			return new EncryptionResult("decrypted text");
+			StringBuilder builder = new();
+
+			for (int i = 0; i < encryptedText.Length; i += 3)
+			{
+				int number = int.Parse(encryptedText.Substring(i, 3));
+				foreach (var kvp in encryptionKey.Key)
+				{
+					if (kvp.Value.Contains(number))
+					{
+						builder.Append(kvp.Key);
+						break;
+					}
+				}
+			}
+
+			return new EncryptionResult(builder.ToString());
+		}
+
+		private static Dictionary<char, List<int>> GenerateKeyCopy(
+			HomophonicEncryptionKey encryptionKey)
+		{
+			Dictionary<char, List<int>> keyCopy = [];
+			foreach (var kvp in encryptionKey.Key)
+				keyCopy[kvp.Key] = [.. kvp.Value];
+
+			return keyCopy;
 		}
 	}
 }
