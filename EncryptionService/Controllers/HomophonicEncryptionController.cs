2ecb0615044272277f68a4f49ce60783cf947cee
Bohdan Kalnytskyi <bogdan201375@gmail.com>
2025-03-31 13:18:55 +0300

    The initial view and the controller are created.

---
+++ EncryptionService/Controllers/HomophonicEncryptionController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+using EncryptionService.Configurations;
+using EncryptionService.Core.Interfaces;
+using EncryptionService.Core.Models;
+using EncryptionService.Core.Models.BlockTransposition;
+using EncryptionService.Models;
+
+namespace EncryptionService.Controllers
+{
+	public class HomophonicEncryptionController(
+		IEncryptionService<EncryptionResult, BlockTranspositionKey, int[]> encryptionService,
+		IOptions<EncryptionSettings> encryptionSettings)
+		: Controller
+	{
+		readonly IEncryptionService<EncryptionResult, BlockTranspositionKey, int[]>
+			_encryptionService = encryptionService;
+		readonly EncryptionSettings _encryptionSettings = encryptionSettings.Value;
+
+		public IActionResult Index() => View();
+
+		[HttpPost]
+		public IActionResult Index(EncryptionViewModel<EncryptionResult> encryptionViewModel,
+			string actionType)
+		{
+			if (!ModelState.IsValid)
+				return View(encryptionViewModel);
+
+			BlockTranspositionKey key = _encryptionSettings.BlockTranspositionKey;
+			EncryptionResult encryptionResult;
+
+			if (actionType == "Encrypt")
+			{
+				encryptionResult = _encryptionService.Encrypt(encryptionViewModel.InputText!, key);
+				encryptionViewModel.EncryptionResult = encryptionResult;
+			}
+			else if (actionType == "Decrypt")
+			{
+				encryptionResult = _encryptionService.Decrypt(
+					encryptionViewModel.EncryptedInputText!, key);
+				encryptionViewModel.DecryptionResult = encryptionResult;
+			}
+
+			return View(encryptionViewModel);
+		}
+	}
+}
