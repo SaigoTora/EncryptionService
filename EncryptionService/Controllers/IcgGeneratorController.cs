5e90cd0cf4643651153b942b7bbbd4e5d6abe941
Bohdan Kalnytskyi <bogdan201375@gmail.com>
2025-04-14 22:07:20 +0300

    The initial view and the controller are created.

---
+++ EncryptionService/Controllers/IcgGeneratorController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using EncryptionService.Models;
+
+namespace EncryptionService.Controllers
+{
+	public class IcgGeneratorController : Controller
+	{
+		public IActionResult Index() => View();
+
+		[HttpPost]
+		public IActionResult Index(NumberGeneratorViewModel numberGeneratorViewModel)
+		{
+			if (!ModelState.IsValid)
+				return View();
+
+			int generatedNumber = 1;
+			numberGeneratorViewModel.ResultNumber = generatedNumber;
+
+			return View(numberGeneratorViewModel);
+		}
+	}
+}
---
+++ EncryptionService/Models/NumberGeneratorViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace EncryptionService.Models
+{
+	public class NumberGeneratorViewModel
+	{
+		public int Seed { get; set; }
+		public int ResultNumber { get; set; }
+	}
+}
